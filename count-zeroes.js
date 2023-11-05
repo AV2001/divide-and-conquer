8eb9a2213a877987b6e6d859fc8bf14a472128a1
Anirudh Vadlamani <anirudhvadlamani2001@gmail.com>
2023-11-05 11:42:37 +0400

    implement countZeroes function

--- count-zeroes.js
+++ count-zeroes.js
@@ -1,5 +1,23 @@
-function countZeroes() {
-    
+function countZeroes(arr) {
+    let leftIndex = 0;
+    let rightIndex = arr.length - 1;
+
+    while (leftIndex <= rightIndex) {
+        const middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+        const middleElement = arr[middleIndex];
+
+        if (middleElement === 1) {
+            leftIndex = middleIndex + 1;
+        } else if (middleElement === 0) {
+            if (middleIndex === 0 || arr[middleIndex - 1] === 1) {
+                return arr.length - middleIndex;
+            } else {
+                rightIndex = middleIndex - 1;
+            }
+        }
+    }
+
+    return 0;
 }
 
 module.exports = countZeroes;
