2bf59378f63e5eab0f6c8e09d892f6e0165aa18b
Anirudh Vadlamani <anirudhvadlamani2001@gmail.com>
2023-11-07 21:56:22 +0400

    implement functionality for findRotationCount

--- find-rotation-count.js
+++ find-rotation-count.js
@@ -1,5 +1,25 @@
-function findRotationCount() {
-  
+function findRotationCount(arr) {
+    let leftIndex = 0;
+    let rightIndex = arr.length - 1;
+
+    while (leftIndex < rightIndex) {
+        const middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+
+        if (arr[middleIndex] < arr[middleIndex - 1]) {
+            return middleIndex;
+        } else if (arr[middleIndex] > arr[rightIndex]) {
+            leftIndex = middleIndex + 1;
+        } else {
+            rightIndex = middleIndex;
+        }
+    }
+
+    // If the array is not rotated, leftIndex will be at the first element
+    if (leftIndex === 0) {
+        return 0;
+    }
+
+    return leftIndex;
 }
 
-module.exports = findRotationCount
+module.exports = findRotationCount;
