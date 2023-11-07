5e8c6c73fe1d8975d354d2214d344d7ab36583a0
Anirudh Vadlamani <anirudhvadlamani2001@gmail.com>
2023-11-07 22:52:35 +0400

    implement functionality for findFloor

--- find-floor.js
+++ find-floor.js
@@ -1,5 +1,22 @@
-function findFloor() {
-  
+function findFloor(arr, num) {
+    let leftIndex = 0;
+    let rightIndex = arr.length - 1;
+    let floorValue = -1;
+
+    while (leftIndex <= rightIndex) {
+        const middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+
+        if (arr[middleIndex] === num) {
+            return arr[middleIndex];
+        } else if (arr[middleIndex] < num) {
+            floorValue = arr[middleIndex];
+            leftIndex = middleIndex + 1;
+        } else {
+            rightIndex = middleIndex - 1;
+        }
+    }
+
+    return floorValue;
 }
 
-module.exports = findFloor
+module.exports = findFloor;
